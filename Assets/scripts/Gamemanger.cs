4bc02083cabef380d8d413fe7784619be7ecd604
zheng31 <qoo11747@gmail.com>
2018-11-14 01:40:43 +0800

    1

---
+++ Assets/scripts/Gamemanger.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gamemanger : MonoBehaviour
+{
+
+    public GameObject UIRoot = null;
+    public GameObject PoolRoot = null;
+    public GameObject DispRoot = null;
+
+    private List<GameObject> ModelList = new List<GameObject>();
+    private int CurrentPosition = 0; //目前model的位置
+
+    void Start()
+    {
+
+        //scan UI 
+        if (UIRoot != null)
+        {
+            int childCount = UIRoot.transform.childCount;
+            for (int i = 0; i < childCount; i++)
+            {
+                GameObject child = UIRoot.transform.GetChild(i).gameObject;
+                if (child.name.Equals("Buttonleft"))
+                {
+                    Button button = child.GetComponent<Button>();
+                    button.onClick.AddListener(ButtonleftOnClick);
+                }
+
+                if (child.name.Equals("Buttonright"))
+                {
+                    (child.GetComponent<Button>())
+                        .onClick
+                        .AddListener(ButtonrightOnClick);
+                }
+
+                if (child.name.Equals("back"))
+                {
+                    (child.GetComponent<Button>())
+                        .onClick
+                        .AddListener(backOnClick);
+                }
+            }
+        }
+
+
+        //scan pool root ,put model into list
+        if (PoolRoot != null)
+        {
+            int childCount = PoolRoot.transform.childCount;
+            for (int i = 0; i < childCount; i++)
+            {
+                ModelList.Add(PoolRoot.transform.GetChild(i).gameObject);
+            }
+        }
+
+        Display();
+
+    }
+    private void Display()
+    {
+        foreach (GameObject g in ModelList)
+        {
+            g.transform.parent = PoolRoot.transform; //all reset
+        }
+        Debug.Log("cp = "+CurrentPosition);
+
+        ModelList[CurrentPosition].transform.parent = DispRoot.transform;  //瞬移
+        ModelList[CurrentPosition].transform.localPosition = Vector3.zero; //reset
+    }
+    public void ButtonleftOnClick()
+    {
+        Debug.Log("ButtonleftOnClick");
+
+        CurrentPosition--;
+        if (CurrentPosition < 0)
+        {
+            CurrentPosition = 0;
+        }
+        Display();
+    }
+    public void ButtonrightOnClick()
+    {
+        Debug.Log("ButtonrightOnClick");
+
+        CurrentPosition++;
+        if (CurrentPosition >= ModelList.Count)
+        {
+            CurrentPosition = ModelList.Count - 1;
+        }
+        Display();
+    }
+
+    public void backOnClick()
+    {
+        Debug.Log("backOnClick");
+    }
+
+}
+
---
+++ Assets/scripts/handler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class handler : MonoBehaviour {
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 vec3 = this.transform.eulerAngles;
+        vec3.y += 20 * Time.deltaTime;
+        vec3.y %= 360;
+        this.transform.eulerAngles = vec3;
+    }
+}
+
