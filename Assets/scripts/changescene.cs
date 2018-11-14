c6f694d76406b14795c3ac7a41c08bd0bcfab880
zheng31 <qoo11747@gmail.com>
2018-11-14 14:36:41 +0800

    更新

---
+++ Assets/scripts/changescene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changescene : MonoBehaviour
+{
+    public void SceneLoader(int SceneIndex)
+    {
+        SceneManager.LoadScene(SceneIndex);
+    }
+}
